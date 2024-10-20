f2b5c000221b6ba9985761ffa6888992cc0a19fc
andria-goodwin <143760838+andria-goodwin@users.noreply.github.com>
2024-10-20 13:20:57 -0500

    created wind, moon phase, and sunrise/sunset sections

--- client/src/components/MoonPhase.jsx
+++ client/src/components/MoonPhase.jsx
@@ -1,1 +1,45 @@
-// Moon phase
+'use client'
+
+// Moon Phase
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import Fade from '@mui/material/Fade';
+import Image from 'next/image';
+import fullMoon from '@/public/images/full-moon.png'
+
+const card = (
+    <React.Fragment>
+      <CardHeader title="Moon Phase"/>
+      <CardContent >
+        <Fade in={true} timeout={5000}>
+            <Image
+                src={fullMoon}
+                height={200}
+                width={200}
+                alt="Moon Phase"
+            />
+        </Fade>
+        <br/>
+        <br/>
+        Current Phase: Full Moon
+        <br/>
+        Moonrise: 8:58 PM
+        <br/>
+        Next Full Moon: Friday, Nov 15 2024
+        <br/>
+        Next New Moon: Friday, Nov 1 2024
+      </CardContent>
+    </React.Fragment>
+  );
+  
+  export default function MoonPhase() {
+    return (
+      <Box>
+        <Card variant="outlined">{card}</Card>
+      </Box>
+    );
+  }
--- client/src/components/SunriseSunset.jsx
+++ client/src/components/SunriseSunset.jsx
@@ -1,1 +1,50 @@
+'use client'
+
 // Sunrise and Sunset
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import Lottie from "lottie-react";
+import sunriseAnimation from '@/public/images/sunrise-animation.json';
+import sunsetAnimation from '@/public/images/sunset-animation.json';
+import { Grid2 } from '@mui/material';
+
+const Sunrise = () => <Lottie animationData={sunriseAnimation} loop={true} />;
+
+const Sunset = () => <Lottie animationData={sunsetAnimation} loop={true} />;
+
+const card = (
+        <React.Fragment>
+            <CardHeader title="Sunrise & Sunset"/>
+            <CardContent >
+                <Grid2 container spacing={1}>
+                    <Grid2 size={6}>
+                        <Sunrise />
+                        <br/>
+                        Sunrise
+                        <br/>
+                        7:43 AM
+                    </Grid2>
+                    <Grid2 size={6}>
+                        <Sunset/>
+                        <br/>
+                        Sunset
+                        <br/>
+                        6:44 PM
+                    </Grid2>
+                </Grid2>
+            </CardContent>
+        </React.Fragment>
+
+);
+  
+  export default function SunriseSunset() {
+    return (
+      <Box>
+        <Card variant="outlined">{card}</Card>
+      </Box>
+    );
+  }
